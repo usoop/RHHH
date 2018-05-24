9c312a90d65de09bceb1ce8caceba5d1dd48c8f5
Tommy Fan <lecheng.fan@stanford.edu>
2018-05-24 19:49:30 +0000

    Change naming scheme

--- plot_graph.py
+++ plot_graph.py
@@ -31,7 +31,7 @@ def extract_coverage_error_rates(results_obj):
 	    packet_num_to_coverage[packet_num] = confidence_interval_95(error_rates)
 	return packet_num_to_coverage
 
-def graph(rhh_confidence_intervals, _10_rhh_confidence_intervals):
+def graph(rhh_confidence_intervals, _10_rhh_confidence_intervals, graph_name):
 	x_val = [rhh_confidence_intervals.keys(), _10_rhh_confidence_intervals.keys()]
 	intervals = [rhh_confidence_intervals.values(), _10_rhh_confidence_intervals.values()]
 	y_val = [[], []]
@@ -42,9 +42,9 @@ def graph(rhh_confidence_intervals, _10_rhh_confidence_intervals):
 	        y_err[i].append((interval[1]-interval[0])/2)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	bp = ax.errorbar(x_val[0], y_val[0], yerr=y_err[0], color="red", marker='o', capsize=10, ls="none")
-	bp = ax.errorbar(x_val[1], y_val[1], yerr=y_err[1], color="blue", marker='o', capsize=10, ls="none")
-	fig.savefig('accuracy_error_fig.png')
+	bp = ax.errorbar(x_val[0], y_val[0], yerr=y_err[0], color="red", marker='o', capsize=5, ls="none")
+	bp = ax.errorbar(x_val[1], y_val[1], yerr=y_err[1], color="blue", marker='o', capsize=5, ls="none")
+	fig.savefig("_".join(graph_name.split()) + '_fig.png')
 
 # Uses the student t distribution to compute a 95% confidence interval
 # @param data
@@ -67,7 +67,7 @@ def main():
 	_10_accuracy_error_rates = extract_accuracy_error_rates(_10_rhhh_results)
 	_10_coverage_error_rates = extract_coverage_error_rates(_10_rhhh_results)
 
-	graph(accuracy_error_rates, _10_accuracy_error_rates)
+	graph(accuracy_error_rates, _10_accuracy_error_rates, "accuracy error rates")
 
 if __name__ == "__main__":
 	main()
