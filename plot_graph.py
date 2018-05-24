9270035905e6fe98ea29a71d106cb6d03e3f1950
Tommy Fan <lecheng.fan@stanford.edu>
2018-05-24 01:28:49 +0000

    Make cap size larger

--- plot_graph.py
+++ plot_graph.py
@@ -4,31 +4,68 @@ mpl.use('agg')
 import numpy as np
 import matplotlib.pyplot as plt
 import pdb
+from parse_results import parse_file
 
-def convert_data(inputs):
-	result = []
-	for trial in inputs:
+import scipy as sp
+import scipy.stats
+
+def extract_accuracy_error_rates(results_obj):
+	packet_num_to_accuracy = {}
+	for packet_num in results_obj:
 	    error_rates = []
-	    for data_point in trial:
-		error_rate = float(data_point[0])/float(data_point[1])
-		error_rates.append(error_rate)
-		
-	    result.append(erorr_rates)
-		
-	
-	return [collectn_1, collectn_2, collectn_3, collectn_4]
+	    for trial in results_obj[packet_num]:
+		accuracy_errors = trial["accuracy_errors"]
+		num_hhh = trial["num_reported_hhh"]
+		error_rates.append(float(accuracy_errors)/float(num_hhh))
+	    packet_num_to_accuracy[packet_num] = confidence_interval_95(error_rates)
+	return packet_num_to_accuracy
 
-def graph(data_points):
+def extract_coverage_error_rates(results_obj):
+	packet_num_to_coverage = {}
+	for packet_num in results_obj:
+	    error_rates = []
+	    for trial in results_obj[packet_num]:
+		coverage_errors = trial["coverage_errors"]
+		num_exact_hhh = trial["num_exact_hhh"]
+		error_rates.append(float(coverage_errors)/float(num_exact_hhh))
+	    packet_num_to_coverage[packet_num] = confidence_interval_95(error_rates)
+	return packet_num_to_coverage
+
+def graph(confidence_intervals):
+	x_val = confidence_intervals.keys()
+	intervals = confidence_intervals.values()
+	y_val = []
+	y_err = []
+	for interval in intervals:
+	    y_val.append((interval[0]+interval[1])/2)
+	    y_err.append((interval[1]-interval[0])/2)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	num_packets = [1e6, 2e6, 4e6, 8e6, 16e6, 32e6]
-	bp = ax.boxplot(data_points, positions=num_packets)
+	#y_err = np.matrix(y_err).transpose()
+	bp = ax.errorbar(x_val, y_val, yerr=y_err, marker='o', capsize=10, ls="none")
 	fig.savefig('fig1.png')
 
+# Uses the student t distribution to compute a 95% confidence interval
+# @param data
+#    A list of data points for which we want to compute the confidence interval
+# @return
+#    A tuple (x, y) representing the lower and upper bounds of the computed interval
+def confidence_interval_95(data):
+        a = 1.0*np.array(data)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * sp.stats.t._ppf((1+.95)/2., n-1)
+        return (m-h, m+h)
+        
 def main():
-	rhhh_accuracy_errors = [[(45,155), (15,177), (8,109), (3,99), (1,90), (1,88)]]
-	data_points = convert_data()
-	graph(data_points)
+	rhhh_results = parse_file("trace_chicago2015_RandHHH2D_output.txt")
+	#10_rhhh_results = parse_file("trace_chicago2015_10RandHHH2D_output.txt")
 
+	accuracy_error_rates = extract_accuracy_error_rates(rhhh_results)
+	coverage_error_rates = extract_coverage_error_rates(rhhh_results)
+	print accuracy_error_rates
+	print coverage_error_rates
+
+	graph(accuracy_error_rates)
 if __name__ == "__main__":
 	main()
