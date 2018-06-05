a898cf3e2e54fbb40232f671c0bde721e160a702
Tommy Fan <lecheng.fan@stanford.edu>
2018-06-05 07:06:36 +0000

    New figs for repro rand hhh

--- chicago2015/plot_graph.py
+++ chicago2015/plot_graph.py
@@ -85,8 +85,8 @@ def confidence_interval_95(data):
         return (m-h, m+h)
         
 def main():
-	rhhh_results = parse_file("trace_chicago2015_RandHHH2D_output.txt")
-	_10_rhhh_results = parse_file("trace_chicago2015_10RandHHH2D_output.txt")
+	rhhh_results = parse_file("trace_chicago2015_repro_RandHHH_output.txt")
+	_10_rhhh_results = parse_file("trace_chicago2015_repro_10RandHHH_output.txt")
 
 	accuracy_error_rates = extract_accuracy_error_rates(rhhh_results)
 	coverage_error_rates = extract_coverage_error_rates(rhhh_results)
